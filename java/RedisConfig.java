e2996d6b37660d41ba8c79647f3fa12034e49de6
SangHoon Kang <53715807+totohoon02@users.noreply.github.com>
2025-12-03 17:19:52 +0900

    Update RedisConfig.java

--- java/RedisConfig.java
+++ java/RedisConfig.java
@@ -1,3 +1,4 @@
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,23 +7,22 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-    
+
     @Value("${spring.data.redis.host}")
     private String host;
-    
+
     @Value("${spring.data.redis.port}")
     private int port;
-    
+
     @Value("${spring.data.redis.password}")
     private String password;
-    
-    
-    
+
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         // host, port가 .yml에서 로드되지 않음
@@ -40,9 +40,20 @@ public class RedisConfig {
         // Key Serializer
         template.setKeySerializer(new StringRedisSerializer());
 
-        // Value Serializer (JSON으로 저장할 경우)
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        // Value Serializer
+        /*
+        * JSON 형식으로 저장
+        * {
+        *   "@class": "java.lang.Long",
+        *   "value": 1000
+        * }
+        * opsForValue로 get, set
+        * Object로 반환, 캐스팅 필요
+        * */
+        template.setValueSerializer(RedisSerializer.json());
 
+        // 내부 유효성 검사 실시
+        template.afterPropertiesSet();
         return template;
     }
 
